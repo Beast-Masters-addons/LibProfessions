9e1fd1e762856a61b926aa50cb3920aa3b295d78
datagutten <datagutten@datagutten.net>
2021-01-09 12:35:45 +0100

    Add tests for GetAllSkills

---
+++ tests/test_LibProfessions.lua
@@ -0,0 +1,35 @@
+local lu = require('luaunit')
+
+loadfile('wow_functions.lua')()
+loadfile('../LibProfessions.lua')()
+
+_G['test'] = {}
+local test = _G['test']
+local lib = _G['LibProfessions']
+
+
+function test:testGetAllSkillsNoFilter()
+    local skills = lib:GetAllSkills()
+--[[    for key, value in pairs(skills) do
+        print(key, value)
+    end]]
+    lu.assertNotNil(skills['Fishing'])
+    lu.assertNotNil(skills['Herbalism'])
+    lu.assertNotNil(skills['Feral Combat'])
+end
+
+function test:testGetSecondarySkills()
+    local skills = lib:GetAllSkills('Secondary Skills')
+    lu.assertNotNil(skills['Fishing'])
+    lu.assertNil(skills['Herbalism'])
+    lu.assertNil(skills['Feral Combat'])
+end
+
+function test:testGetAllProfessions()
+    local skills = lib:GetAllSkills({'Professions', 'Secondary Skills'})
+    lu.assertNotNil(skills['Fishing'])
+    lu.assertNotNil(skills['Herbalism'])
+    lu.assertNil(skills['Feral Combat'])
+end
+
+os.exit(lu.LuaUnit.run())
