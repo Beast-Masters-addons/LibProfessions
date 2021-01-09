1cdeda3fbc6a3ef779761d354ae3ac83b9375f64
datagutten <datagutten@datagutten.net>
2021-01-09 15:47:52 +0100

    Use new version handling class

--- addon/LibProfessions.lua
+++ addon/LibProfessions.lua
@@ -6,8 +6,7 @@ lib.version = '@project-version@'
 lib.utils = _G['BMUtils']
 
 if _G['LibStub'] then
-    lib.utils = LibStub("BM-utils-1", 4)
-    local major, minor = lib:parse_version(lib.version)
+    local major, minor = _G['BMUtils-Version'].parse_version(lib.version)
     lib = _G['LibStub']:NewLibrary("LibProfessions-"..major, minor)
 end
 
