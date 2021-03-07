335df57f7fa8e311a86588aef951d8b930c395ad
datagutten <datagutten@datagutten.net>
2021-03-07 16:13:16 +0100

    Move version selection to separate file

--- tests/test_ProfessionApi.lua
+++ tests/test_ProfessionApi.lua
@@ -1,26 +1,7 @@
 local lu = require('luaunit')
 
 loadfile('wow_functions.lua')()
-
-if os.getenv('CLASSIC_VERSION') ~= nil then
-    print('Running tests for WoW Classic')
-    function _G.GetBuildInfo()
-        return "1.13.2", 32600, "Nov 20 2019", 11302
-    end
-    loadfile('profession_api_classic.lua')()
-else
-    print('Running tests for WoW Retail')
-    function _G.GetBuildInfo()
-        return "9.0.2", 37474, "Feb 3 2021", 90002
-    end
-    loadfile('profession_api_retail.lua')()
-
-    --TODO: Test should be skipped, this function does not exist in WoW Retail
-    function _G.GetNumTradeSkills()
-        return 2
-    end
-
-end
+loadfile('version_select.lua')()
 
 loadfile('build_utils/utils/load_toc.lua')('../LibProfessions.toc')
 
