1d4a8cabadfa1741806f6f08d7e0006b204802df
datagutten <datagutten@datagutten.net>
2021-03-07 16:55:17 +0100

    Check correct key for classic

--- tests/test_CurrentProfession.lua
+++ tests/test_CurrentProfession.lua
@@ -9,6 +9,7 @@ loadfile('version_select.lua')()
 loadfile('build_utils/utils/load_toc.lua')('../LibProfessions.toc')
 
 local lib = _G['CurrentProfession']
+local is_classic = select(4, _G.GetBuildInfo()) < 20000
 
 function test:testGetReagents()
     local reagents = lib:GetReagents(3447)
@@ -18,7 +19,16 @@ end
 function test:testGetRecipes()
     local recipes = lib:GetRecipes()
     lu.assertNotNil(recipes)
-    lu.assertNotNil(recipes[3447])
+    local key
+
+    if is_classic then
+        key = 1
+    else
+        key = 3447
+    end
+
+    lu.assertNotNil(recipes[key])
+    lu.assertEquals('Healing Potion', recipes[key]['name'])
 end
 
 os.exit(lu.LuaUnit.run())
