c1febe0fcbb74e14d4226c067366e5745eae134a
datagutten <datagutten@datagutten.net>
2021-03-07 16:20:11 +0100

    Add links for all reagents

--- tests/profession_api_retail.lua
+++ tests/profession_api_retail.lua
@@ -23,8 +23,16 @@ function _G.C_TradeSkillUI.GetRecipeNumReagents(recipeID)
 end
 
 function _G.C_TradeSkillUI.GetRecipeReagentItemLink(skillId, reagentId)
-    if skillId == 3447 and reagentId == 1 then
-        return '\124cffffffff\124Hitem:2453::::::::60:::::\124h[Bruiseweed]\124h\124r'
+    if skillId == 2 or 3447 then
+        if reagentId == 1 then
+            return '\124cffffffff\124Hitem:2453::::::::60:::::\124h[Bruiseweed]\124h\124r'
+        elseif reagentId == 2 then
+            return '\124cffffffff\124Hitem:2450::::::::60:::::\124h[Briarthorn]\124h\124r'
+        elseif reagentId == 3 then
+            return '\124cffffffff\124Hitem:3372::::::::60:::::\124h[Leaded Vial]\124h\124r;'
+        end
+    else
+        error('Invalid skillId ' .. skillId)
     end
 end
 
@@ -40,13 +48,13 @@ function _G.C_TradeSkillUI.GetRecipeReagentInfo(tradeSkillRecipeId, reagentId)
 end
 
 function _G.C_TradeSkillUI.GetAllRecipeIDs()
-    return {3447}
+    return { 3447 }
 end
 
 function _G.C_TradeSkillUI.GetRecipeInfo(recipeID)
     if recipeID == 3447 then
         return 'Healing Potion'
     else
-        error('Invalid recipeId '..recipeID)
+        error('Invalid recipeId ' .. recipeID)
     end
 end
