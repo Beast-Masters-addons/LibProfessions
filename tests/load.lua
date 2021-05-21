94b2e5cf24097a49c9781854e00edb7439ba95d5
datagutten <datagutten@datagutten.net>
2021-05-21 15:04:17 +0200

    Load skills

--- tests/load.lua
+++ tests/load.lua
@@ -3,5 +3,6 @@ if os.getenv('GAME_VERSION') == 'retail' then
     loadfile('build_utils/wow_api/profession_api_retail.lua')()
 else
     loadfile('build_utils/wow_api/profession_api_classic.lua')()
+    loadfile('build_utils/wow_api/skills.lua')()
 end
 loadfile('build_utils/utils/load_toc.lua')('../LibProfessions.toc')
