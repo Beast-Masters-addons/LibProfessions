16b0cc2c4e6f040ec51bb1b76d827ab7d9ea59dc
datagutten <datagutten@datagutten.net>
2023-03-19 13:20:10 +0100

    Add script to build profession ranks

---
+++ tests/build_profession_ranks.py
@@ -0,0 +1,44 @@
+import os
+
+from build_utils import WoWTables
+
+data_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'data'))
+textures = None
+for version in ['classic', 'wrath', 'retail']:
+    print('Build %s' % version)
+    tables = WoWTables(data_folder=data_folder, game=version)
+    if not textures:
+        textures = tables.art_textures()
+        textures = dict(zip(textures.values(), textures.keys()))
+
+    skills = tables.get_db_table('skillline')
+    abilities = list(tables.get_db_table('skilllineability'))
+
+    profession_ranks = {}
+    profession_info = {'professions': {}}
+
+    for skill in skills:
+        rank = 1
+        if skill['CategoryID'] != '11':
+            continue
+        info = {'name': skill['DisplayName_lang'],
+                'icon': int(skill['SpellIconFileID'])
+                }
+        if info['icon'] != 0:
+            info['iconFile'] = textures[info['icon']]
+        profession_info['professions'][int(skill['ID'])] = info
+
+        profession_ranks[skill['DisplayName_lang']] = {}
+        for ability in abilities:
+            if ability['SkillLine'] != skill['ID']:
+                continue
+            if ability['SupercedesSpell'] == '0':
+                continue
+            if rank == 1:
+                profession_ranks[skill['DisplayName_lang']][1] = int(ability['SupercedesSpell'])
+                rank += 1
+            profession_ranks[skill['DisplayName_lang']][rank] = int(ability['Spell'])
+            rank += 1
+
+    tables.save(profession_ranks, 'ProfessionRanks-' + version, save_json=False)
+    tables.save(profession_info, 'ProfessionInfo-' + version, save_json=False)
